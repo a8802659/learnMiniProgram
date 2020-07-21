240c012e65c91725f04b0db64c1994fe277815d3
a8802659 <8802659@qq.com>
2020-07-21 22:11:39 +0800

    10.网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,47 @@
 // pages/home/home.js
+import request from '../../service/network.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // ---------------------------2.使用封装的网络请求
+    request({
+      url:'http://152.136.185.210:8000/home/multidata'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+    // ---------------------------1.原生网络请求
+    // wx.request({
+      // 1.简单请求
+      // url: 'http://152.136.185.210:8000/home/multidata',
+      // success:function(res){
+      //   console.log(res)
+      // }
+      // 2.get请求携带参数
+      // url: 'http://152.136.185.210:8000/api/n3/home/data',
+      // data:{
+      //   type:'sell',  
+      //   page:1
+      // },
+      // success:function(res){
+      //   console.log(res)
+      // }
+    // })
+     // 3.post请求 并携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method:'post',
+    //   data:{
+    //     name:"codewhy",
+    //     age:"18"
+    //   },
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
   },
 
   /**
