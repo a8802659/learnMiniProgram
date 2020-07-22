0d8d290a3af355e57149258a5aa7b2c48e722534
a8802659 <8802659@qq.com>
2020-07-22 14:28:12 +0800

    10.展示弹窗_页面分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,79 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中...',
+      duration: 3000,
+      icon:'loading', //success loading none
+      // image:'' // 自定义图标的本地路径，image 的优先级高于 icon
+      mask:true,  //是否显示透明蒙层，防止触摸穿透
+      success:function(){
+        console.log("弹窗成功")
+        // wx.hideToast()  可手动设置隐藏
+      },
+      fail:function(){
+        console.log('弹窗失败')
+      },
+      complete:function(){
+        console.log('弹窗完成')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title:'我是标题',
+      content:'我是内容哈哈哈',
+      // showCancel:false,//是否显示取消键
+      cancelText:'取消', 
+      confirmText:'下载',
+      cancelColor:'red',
+      confirmColor:'blue',
+      success:function(res){
+        if(res.cancel){
+          console.log('用户点击了取消')
+        }
+        if(res.confirm){
+          console.log('用户点击了确定')
+        }
+      },
+      fail:function(err){
+        console.log('调用失败')
+      },
+      complete:function(){
+        console.log('调用完成')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoding(){
+    wx.showLoading({ //不会自动消失，可以在success完成加载后调用wx.hideLoading()
+      title: '加载ing',
+      mask:true,
+    })
+    setTimeout(()=>{
+      wx.hideLoading()
+    },1000) 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowActionSheet(){
+    wx.showActionSheet({
+      itemList: ['想吃','不想'],
+      itemColor:'red',
+      success:function(res){
+        switch(res.tapIndex){
+          case 0:
+            console.log('用户想吃');
+            break;
+          case 1:
+            console.log('用户不想');
+            break;
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return {
+      title:'aaa',
+      path:"pages/about/about",
+      imageUrl:'https://www.csdn.net/' 
+    }
   }
 })
